7ae11a271ffa31fcdbb0974e73da0371f8ee5659
Victor <victoraweb@gmail.com>
2016-05-29 15:55:39 -0300

    Todo adapter

---
+++ app/src/main/java/com/augustovictor/add2do/Adapters/TodosAdapter.java
@@ -0,0 +1,71 @@
+package com.augustovictor.add2do.Adapters;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.CheckBox;
+import android.widget.TextView;
+
+import com.augustovictor.add2do.Models.Todo;
+import com.augustovictor.add2do.R;
+
+import java.util.List;
+
+/**
+ * Created by victoraweb on 5/29/16.
+ */
+public class TodosAdapter extends RecyclerView.Adapter<TodosAdapter.ViewHolder> {
+
+    private List<Todo> mTodos;
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        Context context = parent.getContext();
+        LayoutInflater inflater = LayoutInflater.from(context);
+
+        View todoView = inflater.inflate(R.layout.list_item_todo, parent, false);
+
+        ViewHolder viewHolder = new ViewHolder(todoView);
+        return viewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        Todo todo = mTodos.get(position);
+
+        CheckBox doneCk = holder.doneCheckBox;
+        doneCk.setChecked(todo.ismDone());
+
+        TextView titleTv= holder.titleTextView;
+        titleTv.setText(todo.getmTitle());
+
+        TextView descriptionTv = holder.descriptionTextView;
+        descriptionTv.setText(todo.getmDescription());
+
+        TextView creationDateTv = holder.creationDateTextView;
+        creationDateTv.setText(todo.getmCreationDate().toString());
+    }
+
+    @Override
+    public int getItemCount() {
+        return mTodos.size();
+    }
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+
+        public CheckBox doneCheckBox;
+        public TextView titleTextView;
+        public TextView descriptionTextView;
+        public TextView creationDateTextView;
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+            doneCheckBox = (CheckBox) itemView.findViewById(R.id.todo_done_ck);
+            titleTextView = (TextView) itemView.findViewById(R.id.todo_title_tv);
+            descriptionTextView = (TextView) itemView.findViewById(R.id.todo_description_tv);
+            creationDateTextView = (TextView) itemView.findViewById(R.id.todo_date_tv);
+        }
+    }
+}
--- app/src/main/java/com/augustovictor/add2do/Models/Todo.java
+++ app/src/main/java/com/augustovictor/add2do/Models/Todo.java
@@ -1,40 +1,50 @@
 package com.augustovictor.add2do.Models;
 
 import java.util.ArrayList;
+import java.util.Date;
 
 /**
  * Created by victoraweb on 5/29/16.
  */
 public class Todo {
-    private String title;
-    private String description;
-    private boolean done;
+    private String mTitle;
+    private String mDescription;
+    private boolean mDone;
+    private Date mCreationDate;
 
     public Todo() {
     }
 
-    public String getTitle() {
-        return title;
+    public String getmTitle() {
+        return mTitle;
     }
 
-    public void setTitle(String title) {
-        this.title = title;
+    public void setmTitle(String mTitle) {
+        this.mTitle = mTitle;
     }
 
-    public String getDescription() {
-        return description;
+    public String getmDescription() {
+        return mDescription;
     }
 
-    public void setDescription(String description) {
-        this.description = description;
+    public void setmDescription(String mDescription) {
+        this.mDescription = mDescription;
     }
 
-    public boolean isDone() {
-        return done;
+    public boolean ismDone() {
+        return mDone;
     }
 
-    public void setDone(boolean done) {
-        this.done = done;
+    public void setmDone(boolean mDone) {
+        this.mDone = mDone;
+    }
+
+    public Date getmCreationDate() {
+        return mCreationDate;
+    }
+
+    public void setmCreationDate(Date mCreationDate) {
+        this.mCreationDate = mCreationDate;
     }
 
     private static ArrayList<Todo> createTodoList(int numTodos) {
@@ -42,9 +52,10 @@ public class Todo {
 
         for(int i = 0; i < numTodos; i++) {
             Todo todo = new Todo();
-            todo.setTitle("Todo " + i);
-            todo.setDescription("Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.");
-            todo.setDone(i % 2 == 0);
+            todo.setmTitle("Todo " + i);
+            todo.setmDescription("Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.");
+            todo.setmDone(i % 2 == 0);
+            todo.setmCreationDate(new Date());
             todos.add(todo);
         }
         return todos;
