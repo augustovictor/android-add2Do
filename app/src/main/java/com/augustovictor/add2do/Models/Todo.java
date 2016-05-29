5fbff9bd0e826249d1ff34283b9b71efe13deb22
Victor <victoraweb@gmail.com>
2016-05-29 15:37:43 -0300

    Todo list layout

--- app/src/main/java/com/augustovictor/add2do/Models/Todo.java
+++ app/src/main/java/com/augustovictor/add2do/Models/Todo.java
@@ -1,5 +1,7 @@
 package com.augustovictor.add2do.Models;
 
+import java.util.ArrayList;
+
 /**
  * Created by victoraweb on 5/29/16.
  */
@@ -33,5 +35,18 @@ public class Todo {
 
     public void setDone(boolean done) {
         this.done = done;
+    }
+
+    private static ArrayList<Todo> createTodoList(int numTodos) {
+        ArrayList<Todo> todos = new ArrayList<>();
+
+        for(int i = 0; i < numTodos; i++) {
+            Todo todo = new Todo();
+            todo.setTitle("Todo " + i);
+            todo.setDescription("Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.");
+            todo.setDone(i % 2 == 0);
+            todos.add(todo);
+        }
+        return todos;
     }
 }
