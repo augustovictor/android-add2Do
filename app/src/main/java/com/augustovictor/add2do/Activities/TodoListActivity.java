c9b73afa77376d0136fffd5d4d0b45bf17534ebd
Victor <victoraweb@gmail.com>
2016-05-30 20:53:07 -0300

    Activity organised and toolbar added;

--- app/src/main/java/com/augustovictor/add2do/Activities/TodoListActivity.java
+++ app/src/main/java/com/augustovictor/add2do/Activities/TodoListActivity.java
@@ -2,11 +2,17 @@ package com.augustovictor.add2do.Activities;
 
 import android.content.res.Resources;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
+import android.view.MenuItem;
 
 import com.augustovictor.add2do.Adapters.TodosAdapter;
 import com.augustovictor.add2do.Models.Todo;
@@ -18,28 +24,54 @@ import java.util.ArrayList;
 public class TodoListActivity extends AppCompatActivity {
 
     ArrayList<Todo> todos;
+    private DrawerLayout mDrawer;
+    private NavigationView nvDrawer;
+    private ActionBarDrawerToggle drawerToggle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_todo_list);
+        setUpRecyclerView();
+        setUpToolbar();
+        mDrawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        nvDrawer = (NavigationView) findViewById(R.id.navigation_view);
+        setupDrawerContent(nvDrawer);
 
-        RecyclerView rvTodos = (RecyclerView) findViewById(R.id.recycler_view);
+    }
 
-        todos = Todo.createTodoList(20);
+    private void setupDrawerContent(NavigationView navigationView) {
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(MenuItem menuItem) {
+                return false;
+            }
+        });
+    }
 
-        TodosAdapter adapter = new TodosAdapter(todos);
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
 
-        rvTodos.setAdapter(adapter);
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                mDrawer.openDrawer(GravityCompat.START);
+                return true;
+        }
 
-        rvTodos.setLayoutManager(new LinearLayoutManager(this));
+        return super.onOptionsItemSelected(item);
+    }
 
-        rvTodos.setHasFixedSize(true);
+    @Override
+    protected void onPostCreate(@Nullable Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+    }
 
-        RecyclerView.ItemDecoration itemDecoration = new DividerItemDecoration(this, DividerItemDecoration.VERTICAL_LIST);
-
-        rvTodos.addItemDecoration(itemDecoration);
-
+    public void setUpToolbar() {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
 
         Resources res = getResources();
@@ -49,9 +81,22 @@ public class TodoListActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
+    public void setUpRecyclerView() {
+
+        // RecyclerView
+        RecyclerView rvTodos = (RecyclerView) findViewById(R.id.recycler_view);
+        todos = Todo.createTodoList(20);
+
+        // Adapter
+        TodosAdapter adapter = new TodosAdapter(todos);
+        rvTodos.setAdapter(adapter);
+        rvTodos.setLayoutManager(new LinearLayoutManager(this));
+
+        // ItemDecoration
+        RecyclerView.ItemDecoration itemDecoration = new DividerItemDecoration(this, DividerItemDecoration.VERTICAL_LIST);
+        rvTodos.addItemDecoration(itemDecoration);
+        rvTodos.setHasFixedSize(true);
+
+
     }
 }
