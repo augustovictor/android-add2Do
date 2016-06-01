b0bcf18360a3259fe18ff4ba3f56531645936200
Victor <victoraweb@gmail.com>
2016-05-31 23:56:36 -0300

    Changing structure to start TodoActivity through a fragment;

--- app/src/main/java/com/augustovictor/add2do/Activities/TodoActivity.java
+++ app/src/main/java/com/augustovictor/add2do/Activities/TodoActivity.java
@@ -1,9 +1,12 @@
 package com.augustovictor.add2do.Activities;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 
+import com.augustovictor.add2do.Fragments.TodoFragment;
 import com.augustovictor.add2do.R;
 
 /**
@@ -15,4 +18,11 @@ public class TodoActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.todo_item);
     }
+
+    public static Intent newIntent(Context context) {
+        Intent i = new Intent(context, TodoActivity.class);
+        return i;
+    }
+
+
 }
---
+++ app/src/main/java/com/augustovictor/add2do/Fragments/TodoFragment.java
@@ -0,0 +1,14 @@
+package com.augustovictor.add2do.Fragments;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by victoraweb on 5/31/16.
+ */
+public class TodoFragment extends Fragment {
+
+    public static TodoFragment newInstance() {
+        TodoFragment fragment = new TodoFragment();
+        return fragment;
+    }
+}
